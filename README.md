27edc4515b78286f64ff5d8546871ba56f017adf
tejakummarikuntla <teja.kummarikuntla@gmail.com>
2019-01-28 23:02:34 +0530

    add region_of_intrest()

--- README.md
+++ README.md
@@ -41,7 +41,32 @@ gives you the total number of pixels in the image.
 - recommended ratio **1:2** or **1:3**
 ![Appled Canny function](Canny_image.PNG)
 - Gradients that exceed the `high_threshold` are traced as bright pixels, identifying the adjacent pixels in the image with the most rapid changes in brightness, small changes in brightness are not traced and accordingly they are black as they fall below the lower threshold 
-- We used `Canny Method ` to outline the strongest gradient in our Image 	  
+- We used `Canny Method ` to outline the strongest gradient in our Image 	
+
+# Identifying Lane lines in the image
+### Region of Intrest()  
+Before we detect. Firstly, we need to decide a particular lanes in the image that we should work-on to write the program. </br>
+In my case it's as shown:
+
+![Decide_lane](decide_lane)
+
+- `The region_of_intrest()` returns the enclosed region of our field of view, in which our region is triangular in shape
+![region_of_intrest](region_of_intrests)
+```
+def region_of_intrest(image): 
+    height = image.shape[0]
+    polygons = np.array([
+    [(200, height), (1100, height), (550, 250)]
+    ])
+    mask = np.zeros_like(image)
+    cv2.fillPoly(mask, polygons, 255)
+    return mask
+
+```
+- var `polygons` get the region of the intrest by giving the co-ordinates (x,y)
+- var `mask` has matrix created by `np.zereos_lie(image)` which means it creates an array with complete zeros which has same shape of the image's corrosponding array. Both array will have the same no.of `rows` and `coloumns`, which means mask has the same amount of pixels and dimensions of our canny_image. [pixels of mask are completely black as the elements are completely zeros]
+- Now, we have to fill the mask with the polygons with the help of openCV's `fillPolly()` function, the third argument represents the colour of our polygon  	
+![region_of_intrest_output](region_of_intrest_output)
 
 ```cv2.imshow('result',blur_image)``` </br>
 #waits for any keyPress to close. </br>
--- lanes.py
+++ lanes.py
@@ -1,36 +1,26 @@
 import cv2
 import numpy as np
 
-#-----STEPS--------
-# 1. Changing the image in to gray Scale
-# 2. (Reduce Noise) applying guassian blue on the image
-# 3. Apply canny method to identify edges in an image
+def canny(image):
+    gray_image = cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)
+    blur_image = cv2.GaussianBlur(gray_image, (5,5), 0)
+    canny_image = cv2.Canny(blur_image,50, 150)
+    return canny_image
+
+def region_of_intrest(image): 
+    height = image.shape[0]
+    polygons = np.array([
+    [(200, height), (1100, height), (550, 250)]
+    ])
+    mask = np.zeros_like(image)
+    cv2.fillPoly(mask, polygons, 255)
+    return mask
+
 
 image = cv2.imread('test_image.jpg')
-#making a copy of image in array rep
 lane_image = np.copy(image)
-#converting the image in to gray sclae for reducing the channels
-#gray scale images only has single channel, while RGB has three channesl
-gray_image = cv2.cvtColor(lane_image,cv2.COLOR_RGB2GRAY)
+canny = canny(lane_image)
 
-#we need to clear the noise in the image, because Image noise can create false noise
-# ultmately affects edge detection.
-#thats smooting the image.
-#filtering and smoothing the image can be done by guassian filter
-
-#to smooth the image --------------
-# we can smooth by modifing the value of a pixel by average value of the
-#pixel intensities around the target pixel.
-#averaging out the pixels in the image to reduce noise is done by a kernal.
-blur_image = cv2.GaussianBlur(gray_image, (5,5), 0)
-#But, when we apply a canny funtion we don't to apply GaussianBlur specially,
-#because cany funtion internally applies it.
-
-#applying Canny method to identify edges.
-#The change in brightness over a series of pixels is the GRADIENT.
-#A strong gradinent indicates a deep change and a small gradient indicates a slight change
-canny_image = cv2.Canny(blur_image,50, 150)
-
-cv2.imshow('result',canny_image)
-#waits for any keyPress to close.
+# cv2.imshow('result',canny)
+cv2.imshow("result",region_of_intrest(canny))
 cv2.waitKey(0)
