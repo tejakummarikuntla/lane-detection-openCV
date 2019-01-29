976170052c0136fc7ad1ae6c90e4c7e5a750a054
tejakummarikuntla <teja.kummarikuntla@gmail.com>
2019-01-30 00:10:37 +0530

    source_code

--- lanes.py
+++ lanes.py
@@ -1,6 +1,34 @@
 import cv2
 import numpy as np
 
+def make_coordinates(image, line_parameters):
+    slope, intercept = line_parameters
+    y1 = image.shape[0]
+    y2 = int(y1*(3/5))
+
+    x1 = ((y1-intercept)/slope)
+    x2 = ((y2-intercept)/slope)
+    return np.array([x1, x2, y1, y2])
+
+
+def average_slope_intercept(image, lines):
+    left_fit = []
+    right_fit = []
+    for line in lines:
+        x1, y1, x2, y2 = line.reshape(4)
+        parameters = np.polyfit((x1,x2), (y1,y2), 1)
+        slope = parameters[0]
+        intercept = parameters[1]
+        if  slope < 0:
+            left_fit.append((slope, intercept))
+        else:
+            right_fit.append((slope, intercept))
+    left_fit_average = np.average(left_fit, axis=0)
+    right_fit_average = np.average(right_fit, axis=0)
+    left_line = make_coordinates(image, left_fit_average)
+    right_line = make_coordinates(image, right_fit_average)
+    return np.array([left_line, right_line])
+
 def canny(image):
     gray_image = cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)
     blur_image = cv2.GaussianBlur(gray_image, (5,5), 0)
@@ -10,8 +38,7 @@ def canny(image):
 def display_lines(image, lines):
     line_image = np.zeros_like(image)
     if lines is not None:
-        for line in lines:
-            x1, y1, x2, y2 = line.reshape(4)
+        for x1, y1, x2, y2 in lines:
             cv2.line(line_image, (x1,y1), (x2,y2), (255, 0, 0), 10)
     return line_image
 
@@ -31,9 +58,10 @@ lane_image = np.copy(image)
 canny = canny(lane_image)
 cropped_image = region_of_intrest(canny)
 lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5 )
-line_image = display_lines(lane_image, lines)
+averaged_lines = average_slope_intercept(lane_image, lines)
+line_image = display_lines(lane_image, averaged_lines)
 combo_imge = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
 # cv2.imshow('result',canny)
-cv2.imshow("result",combo_imge)
+cv2.imshow("result",line_image)
 
 cv2.waitKey(0)
